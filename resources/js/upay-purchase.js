8e48c0a2bb66f9ed0f609b5abd88f8d2fc880d76
cvaughn42 <cvaughn42@comcast.net>
2016-09-22 16:42:06 -0400

    Purchase page stuff
    Signed-off-by: cvaughn42 <cvaughn42@comcast.net>

--- resources/js/upay-purchase.js
+++ resources/js/upay-purchase.js
@@ -7,12 +7,54 @@ app.config(function ($routeProvider) {
 
 app.controller('purchaseController', function($scope, $http, $routeParams) {
     
-    // Get the listing
-    $http("/listing/" + $routeParams.listingId, function(data) {
-        $scope.listing = data;
+    $scope.rejectFee = function() {
+        alert("Too bad - this is u*Pay, not e*Bay!");
+    };
+
+    $scope.getSellerName = function(user) {
+
+        var name = "";
+        var sp = "";
+
+        if (user)
+        {
+            if (user.firstName)
+            {
+                name += user.firstName;
+                sp + " ";
+            }
+                
+            if (user.middleName)
+            {
+                name += sp + user.middleName;
+                sp + " ";
+            }
+
+            if (user.lastName)
+            {
+                name += sp + user.lastName;
+                sp + " ";
+            }
+        }
+
+        return name;
+    };
+
+    $scope.$on('$viewContentLoaded', function (event) {
+
+        // Get the listing from the server to make sure you have the latest info
+        $http.get("/listing/" + $routeParams.listingId).success(function(data) {
+            
+            $scope.listing = data;
+
+            console.dir(data);
+        
+        }).error(function(err) {
+        
+            alert("Error " + err);
+        });
+
+        // Get the user's information
     });
-
-    // Get the user's information
-
     
 });
